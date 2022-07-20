90154259398a49b7fb3002be19fe323f532a5524
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-07-20 23:04:39 +0200

    Footer finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
-import './footer.css'
+import {BsLinkedin} from "react-icons/bs"
+import {FaGithub} from "react-icons/fa"
 
-function Footer() {
+import './footer.css'
+const Footer = () => {
   return (
-    <section id="footer">Footer</section>
+    <footer>
+      <a href="#" className='footer__logo'>TAHA</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/tahatoufik/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/TOUFIK-Taha98/" target="_blank"><FaGithub/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Taha TOUFIK. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
